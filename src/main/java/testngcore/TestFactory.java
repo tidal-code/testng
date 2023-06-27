c22890fd4519a01459dddb2d208248f788f30522
philipkurian <philipkurian@icloud.com>
2023-06-28 11:02:12 +1200

    version update and file formatting

--- src/main/java/testngcore/TestFactory.java
+++ src/main/java/testngcore/TestFactory.java
@@ -1,7 +1,7 @@
 package testngcore;
 
+import com.tidal.utils.propertieshandler.PropertiesFinder;
 import com.tidal.wave.exceptions.RuntimeTestException;
-import com.tidal.wave.propertieshandler.PropertiesFinder;
 import org.testng.*;
 import org.testng.internal.Configuration;
 import org.testng.xml.XmlPackage;
@@ -20,32 +20,31 @@ public class TestFactory implements ITestRunnerFactory {
     @Override
     public TestRunner newTestRunner(ISuite iSuite, XmlTest xmlTest, Collection<IInvokedMethodListener> collection, List<IClassListener> list) {
         try {
-            String[] includedTestGroups= PropertiesFinder.getProperty("testIncludedGroups").split(",");
-            String[] excludedTestGroups=PropertiesFinder.getProperty("testExcludedGroups").split(",");
-            String[] packagesToRun=PropertiesFinder.getProperty("testPackageNames").split(",");
-            int threadCount=Integer.parseInt(PropertiesFinder.getProperty("threadCount"));
-            int dataProviderThreadCount=Integer.parseInt(PropertiesFinder.getProperty("dataProviderThreadCount"));
+            String[] includedTestGroups = PropertiesFinder.getProperty("testIncludedGroups").split(",");
+            String[] excludedTestGroups = PropertiesFinder.getProperty("testExcludedGroups").split(",");
+            String[] packagesToRun = PropertiesFinder.getProperty("testPackageNames").split(",");
+            int threadCount = Integer.parseInt(PropertiesFinder.getProperty("threadCount"));
+            int dataProviderThreadCount = Integer.parseInt(PropertiesFinder.getProperty("dataProviderThreadCount"));
             XmlSuite xmlSuite = new XmlSuite();
             xmlSuite.setParallel(XmlSuite.ParallelMode.METHODS);
             xmlSuite.setThreadCount(threadCount);
             xmlSuite.setDataProviderThreadCount(dataProviderThreadCount);
             xmlSuite.setName(PropertiesFinder.getProperty("testSuiteName"));
-            XmlTest test=new XmlTest(xmlSuite);
-            for(String testGroup:includedTestGroups){
+            XmlTest test = new XmlTest(xmlSuite);
+            for (String testGroup : includedTestGroups) {
                 test.addIncludedGroup(testGroup);
             }
-            for(String testGroup:excludedTestGroups){
+            for (String testGroup : excludedTestGroups) {
                 test.addExcludedGroup(testGroup);
             }
             ArrayList<XmlPackage> xmlPackages = new ArrayList<XmlPackage>();
-            for(String eachPackage:packagesToRun){
+            for (String eachPackage : packagesToRun) {
                 xmlPackages.add(new XmlPackage(eachPackage));
             }
             test.setPackages(xmlPackages);
             return new TestRunner(new Configuration(), iSuite, test, true, collection, list);
-        }
-        catch (Exception ex){
-            throw new RuntimeTestException("Unable to generate testng runner, exception "+ex.getMessage());
+        } catch (Exception ex) {
+            throw new RuntimeTestException("Unable to generate testng runner, exception " + ex.getMessage());
         }
     }
 
--- src/test/java/com/tidal/tests/Tester.java
+++ src/test/java/com/tidal/tests/Tester.java
@@ -3,28 +3,32 @@ package com.tidal.tests;
 import com.tidal.wave.exceptions.RuntimeTestException;
 import org.testng.annotations.Test;
 
+import static com.tidal.wave.webelement.ElementFinder.find;
+
+
 public class Tester {
 
-    @Test(groups ={ "SmokeTest","RegressionTest"})
-    public void testMethod(){
 
+
+    @Test(groups = {"SmokeTest", "RegressionTest"})
+    public void testMethod() {
         System.out.println("This is a test");
     }
 
-    @Test(groups ={ "SmokeTest","RegressionTest","uitest"})
-    public void testUiMethod(){
-
+    @Test(groups = {"SmokeTest", "RegressionTest", "uitest"})
+    public void testUiMethod() {
+        find("name:q").click();
         System.out.println("This is a UI test");
     }
 
-    @Test(groups ={ "SmokeTest","RegressionTest","uitest"})
-    public void testAnotherUiMethod(){
+    @Test(groups = {"SmokeTest", "RegressionTest", "uitest"})
+    public void testAnotherUiMethod() {
 
         System.out.println("This is a another UI test");
     }
 
-    @Test(groups ={ "SmokeTest","RegressionTest","uitest"})
-    public void testFailingMethod(){
+    @Test(groups = {"SmokeTest", "RegressionTest", "uitest"})
+    public void testFailingMethod() {
         System.out.println("This is a failing UI test");
         throw new RuntimeTestException("Test Failure");
     }
