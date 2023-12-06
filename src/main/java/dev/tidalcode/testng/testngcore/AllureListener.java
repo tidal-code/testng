8da58e9bef98b1603611f85592c69b2c55b0eee5
philipkurian <philipkurian@icloud.com>
2023-12-06 16:13:46 +1300

    updated pom version

--- src/main/java/dev/tidalcode/testng/testngcore/AllureListener.java
+++ src/main/java/dev/tidalcode/testng/testngcore/AllureListener.java
@@ -9,20 +9,30 @@ import io.qameta.allure.model.TestResult;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import dev.tidalcode.testng.utils.TestScenario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
+import java.util.Arrays;
 
 public class AllureListener implements TestLifecycleListener {
+    private static Logger logger = LoggerFactory.getLogger(AllureListener.class);
 
     @Override
     public void beforeTestStop(TestResult result) {
-        if (!result.getParameters().isEmpty()) {
-            result.setName(TestScenario.getTestDescription());
-            TestScenario.removeCurrentTestScenario();
-        }
-        TestInfo.remove(result.getStatus());
-        if (!(result.getFullName().contains("API_") || result.getFullName().contains("DB_")) && (result.getStatus().equals(Status.FAILED) || result.getStatus().equals(Status.BROKEN))) {
-            Allure.addAttachment(result.getName() + "_Failed_Screenshot", new ByteArrayInputStream(((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES)));
+        try {
+            if (!result.getParameters().isEmpty()) {
+                result.setName(TestScenario.getTestDescription());
+                TestScenario.removeCurrentTestScenario();
+            }
+            TestInfo.remove(result.getStatus());
+            if (!(result.getFullName().contains("API_") || result.getFullName().contains("DB_")) && (result.getStatus().equals(Status.FAILED) || result.getStatus().equals(Status.BROKEN))) {
+                if (null != Driver.getDriver()) {
+                    Allure.addAttachment(result.getName() + "_Failed_Screenshot", new ByteArrayInputStream(((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES)));
+                }
+            }
+        } catch (Exception e){
+            logger.info("Exception: {}" , Arrays.toString(e.getStackTrace()));
         }
     }
 }
